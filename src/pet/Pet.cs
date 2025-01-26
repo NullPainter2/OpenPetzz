5ef104d995eb30628f030b01190747469d96c4af
nullpainter <nullpainter@seznam.cz>
2025-01-26 12:11:00 +0100

    Add LNZ parser

--- src/pet/Pet.cs
+++ src/pet/Pet.cs
@@ -13,8 +13,58 @@ public partial class Pet : Node2D
 	{
 		World.pets.Add(this);
 
-		PetRenderer petRenderer = new PetRenderer();
+		// @xxx testing ...
+		{
+
+			var FILE = "./Resource/linez/calico-petz3.lnz"; //"YellowBird.lnz";
+
+			var parsed = new Lnz();
+
+			parsed.Parse(FILE);
+
+			//
+			// Print the result ...
+			//
+
+			foreach (var item in parsed.Eyes)
+			{
+				GD.Print(String.Format("eye = {0},{1},{2}", item.X, item.Y, item.ID));
+			}
+
+			foreach (var item in parsed.Whiskers)
+			{
+				GD.Print(String.Format("whisker = {0},{1},{2}", item.Start, item.End, item.Color));
+			}
+
+			foreach (var item in parsed.EarExtensions)
+			{
+				GD.Print(String.Format("ear extension = {0}", item.Percent));
+			}
+
+			foreach (var item in parsed.Linez)
+			{
+				GD.Print(String.Format("linez = {0},{1},{2},{3},{4},{5},{6}",
+					item.StartBall,
+					item.EndBall,
+					item.FuzzAmount,
+					item.LeftOutlineColor,
+					item.RightOutlineColor,
+					item.StartThickness,
+					item.EndThickness
+				));
+			}
+
+			foreach (var item in parsed.PaintBallz)
+			{
+				GD.Print(String.Format("PaintBallz = {0},{1},{2}",
+					item.Direction.X,
+					item.Direction.Y,
+					item.Direction.Z
+				));
+			}
+		}
 		
+		PetRenderer petRenderer = new PetRenderer();
 		AddChild(petRenderer);
 	}
 
@@ -26,12 +76,9 @@ public partial class Pet : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
 	}
 
 	public override void _Draw()
 	{
-
 	}
-
 }
